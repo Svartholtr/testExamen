f974c28836701b3ce3f75b25bc512e814196c2c0
Christian Bo Laurentinus Skarut <chri23fp@student.eadania.dk>
2024-05-28 09:53:51 +0100

    fjernede mine attributer, for at teste branching fra ADO

--- Character.cs
+++ Character.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestEksamen
-{
     namespace TestEksamen
     {
         public class Character
         {
+        // atribute comment added for pull request test
             public string Name { get; set; }
-            public int Age { get; set; }
+
         }
     }
-}
